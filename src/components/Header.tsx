c82971d23b2a87ac6ae408ea72f2de4350df0758
Lunost <ak56z@mail.ru>
2025-04-07 19:09:15 +0300

    Изменение header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,34 @@
 import Link from "next/link";
+import style from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <header className='bg-amber-400 shadow-sm'>
-            <nav className='container mx-auto px-4 py-5 flex justify-around'>
-                <Link className='text-xl text-white font-bold' href={'/'}>Гид по заведениям</Link>
-                <div className='flex gap-3 *:font-bold'>
-                    <Link className='text-gray-100 px-4 py-2 rounded-2xl hover:bg-amber-500 hover:text-gray-300 hover:shadow-xl focus:outline-2 focus:outline-offset-2 focus:outline-amber-50' href="/users">Users</Link>
-                    <Link className='text-gray-100 px-4 py-2 rounded-2xl hover:bg-amber-500 hover:text-gray-300 hover:shadow-xl focus:outline-2 focus:outline-offset-2 focus:outline-amber-50' href="/places">Posts</Link>
-                    <Link className='text-gray-100 px-4 py-2 rounded-2xl hover:bg-amber-500 hover:text-gray-300 hover:shadow-xl focus:outline-2 focus:outline-offset-2 focus:outline-amber-50' href="/categories">Categories</Link>
-                </div>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className={style.header}>
+      <nav className={style.nav}>
+        <Link className={style.icon} href={"/"}></Link>
+        <div className={style.links}>
+          {/* <Link className='' href="/users">О НАС</Link>
+                    <Link className='' href="/places">Posts</Link>
+                    <Link className='' href="/categories">Categories</Link> */}
+          <Link className={style.link} href="#">
+            О НАС
+          </Link>
+          <Link className={style.link} href="#">
+            МЕРОПРИЯТИЯ
+          </Link>
+          <Link className={style.link} href="#">
+            БЛОГ
+          </Link>
+          <Link className={style.link} href="#">
+            ВХОД
+          </Link>
+          <Link className={style.lr} href="#">
+            РЕГИСТРАЦИЯ
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
